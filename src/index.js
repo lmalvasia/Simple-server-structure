c392ddcd11c3e51b1d121d25ce941c534e9243ad
Lucas Malvasia <lucas.malvasia@climate.com>
2020-12-22 18:58:42 -0300

    server static

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+app.use(express.static('public'));
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useNewUrlParser', true);
