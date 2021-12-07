314a19a526a6c8e44b033d9a02cf76b1916753d9
Nico Viola <naav02@gmail.com>
2021-12-07 18:53:04 -0300

    BaSD-2021: class 13 config

--- src/routes/charactersRoutes.js
+++ src/routes/charactersRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const charactersController = require('../controllers/charactersController');
-const authMiddleware = require('../middlewares/authMiddleware');
+// const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router
-  .get('/', authMiddleware, charactersController.getAllCharacters)
-  .post('/', authMiddleware, charactersController.createCharacter)
-  .delete('/:id', authMiddleware, charactersController.deleteCharacter);
+  .get('/', charactersController.getAllCharacters)
+  .post('/', charactersController.createCharacter)
+  .delete('/:id', charactersController.deleteCharacter);
+// .get('/', authMiddleware, charactersController.getAllCharacters)
+// .post('/', authMiddleware, charactersController.createCharacter)
+// .delete('/:id', authMiddleware, charactersController.deleteCharacter);
 
 module.exports = router;
